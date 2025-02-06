df38c04f4b121bd4a868c9a50b05a2d756da5358
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2025-02-06 21:05:41 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  console.log(event);
+  event.preventDefault();
+
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  };
+
+  const data = {
+    email: email,
+    password: password,
+  };
+
+  console.log(data);
+
+  form.reset();
+};
