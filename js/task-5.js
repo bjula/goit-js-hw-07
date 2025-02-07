20d0bfa9bc6cff8fc1f959d8360262120099d351
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2025-02-07 17:30:09 +0200

    add task-5 js

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777216)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const buttonElem = document.querySelector('.change-color');
+const bodyElem = document.querySelector('body');
+const spanElem = document.querySelector('.color');
+
+buttonElem.addEventListener('click', onBtnClick);
+
+function onBtnClick(e){
+  bodyElem.style.backgroundColor = getRandomHexColor();
+  spanElem.textContent = `${bodyElem.style.backgroundColor}`
+
+};
